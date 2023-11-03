d563470e9164f729d2bb1084ace8336d4e57d5b8
Felipe Costa <felipecosta8449@hotmail.com>
2023-11-03 08:01:09 -0300

    update notebook and notes

--- Q-Learning_Auctions/notebook.py
+++ Q-Learning_Auctions/notebook.py
@@ -1,30 +1,106 @@
+import random
 import numpy as np
 import pandas as pd
 import altair as alt
 
-m = 9
-bids = [i / (m + 1) for i in range(m)]
+random_int
+n_actions = 10
+actions = [i / 10 for i in range(n_actions + 1)]
+
+# action: proportion of the private value: a \in [0,1]
+# state: (number of bidders, private value, winning bid - bid,
+Q = np.zeros((10, 10))
+random.random()
 
 
-# ## The Model
-#
-# Two bidders participate in a sequence of auctions. In every period $t \in \left\{1, \dots,\infty\right\}$ an auctioneer runs an auction to allocate a single non-divisible object to one of the bidders. Both bidders value the object at $v _{i}=1$ and the value is constant over time.
+class bidder:
+    def __init__(self, id, alpha, gamma, epsilon):
+        self.id = id
+        self.private_value = random.randint(0, 100)
+        self.alpha = alpha
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.actions = []
+        self.bids = []
+        self.states = []
+        self.rewards = []
+        self.Q = np.zeros((10, 11))
 
-class bidder():
+    def policy(self, state):
+        if random.random() < self.epsilon:
+            action = random.randint(0, 10) / 100
+            # self.actions.append(action)
+            return action
+        else:
+            action = np.argmax(self.Q[state, :]) / 100
+            # self.actions.append(action)
+            return action
 
-    def __init__(self, private_value):
+    def q_update(self):
+        if len(self.states) > 1:
+            state = self.states[-2]
+            next_state = self.states[-1]
+            reward = self.rewards[-2]
+            action = int(self.actions[-2] * 100)
+            self.Q[state, action] = (1 - self.alpha) * self.Q[
+                state, action
+            ] + self.alpha * (reward + self.gamma * np.max(self.Q[next_state, :]))
 
-        self.private_value = private_value
 
-    def policy(self, observation):
-
-class auction():
-    def __init__(self):
+class auction:
+    def __init__(self, n_bidders, alpha, gamma, epsilon):
         self.auctions = {}
+        self.bidders = [bidder(i, alpha, gamma, epsilon) for i in range(n_bidders)]
 
-    def run(self, periods, bidders_values, auction_alpha):
+    def winner_rule(self):
+        bids = [b.actions[-1] for b in self.bidders]
+        return np.argmax(bids), np.max(bids)
+
+    def run(self, periods, auction_alpha):
         for t in range(periods):
-            self.auctions[t] = auction_period(bidders_values, auction_alpha)
+            for b in self.bidders:
+                if t != 0:
+                    action = b.policy(b.states[-1])  # * b.private_value
+                    bid = action * b.private_value
+                    b.actions.append(action)
+                    b.bids.append(bid)
+                else:
+                    action = random.randint(0, 10) / 100  # * b.private_value
+                    bid = action * b.private_value
+                    b.actions.append(action)
+                    b.bids.append(bid)
+
+            winner_bidder, winning_bid = self.winner_rule()
+
+            for b in self.bidders:
+                if b.id == winner_bidder:
+                    reward = b.private_value - winning_bid
+                    state = 0
+                    b.states.append(state)
+                    b.rewards.append(reward)
+                    b.q_update()
+
+                else:
+                    reward = 0
+                    state = winning_bid - b.bids[-1]
+                    b.states.append(state)
+                    b.rewards.append(reward)
+                    b.q_update()
+
+
+n_bidders = 2
+alpha = 0.4
+gamma = 0.9
+epsilon = 0.05
+a = auction(n_bidders, alpha, gamma, epsilon)
+a.run(10, auction_alpha=1)
+a.bidders[1].states
+a.bidders[1].private_value
+a.bidders[0].bids
+a.bidders[1].bids
+
+teste = 0 - 100
+round(np.abs(teste))
 
 
 #
--- Q-Learning_Auctions/notes.md
+++ Q-Learning_Auctions/notes.md
@@ -41,10 +41,20 @@ $$
 
 ## Paper Comments
 
-I want to stop in this point of the paper. In the bandit formulation, a reward maximizing agent interacts with a environment through an action, receiveing a reward, in each step, that is a function of this action. Through repetition, the agent trys to 'discover' the reward distribution (this can be done through a set of different algorithms), and then, when the agent is confident that she has found the optimal action (or optimal policy), she exploit it.
+I want to stop at this point of the paper. In the bandit formulation, a reward maximizing agent interacts with a environment through an action, receiveing a reward, in each step, that is a function of this action. Through repetition, the agent trys to 'discover' the reward distribution (this can be done through a set of different algorithms), and then, when the agent is confident that she has found the optimal action (or optimal policy), she exploit it.
 
 In the simplest formulation, the bandit problem doesn't have a state (or you could think of a problem with a single repeting state in each step, making it redundant). A more broad bandit problem incorporates states (also called context in the bandit literature), giving additional information to the agent of what is the reward distribution that she is interacting with. This formulation resembles the RL formulation, except that in the bandit model the states dynamics are independent of the agent's actions.
 
-If you agree with this argument, you could say that the Q-learning algorithm proposed by the authors is in fact a bandit algorithm. But I woundn't go too far to state that the problem is a bandit one, I still think it is a RL problem. But unlike the authors, I think that the problem is mis-specified. First, the expectation in (1) is with respect to something. It could
+If you agree with this argument, you could say that the Q-learning algorithm proposed by the authors is in fact a bandit algorithm. Indeed, considering the first-price auction and the associated reward $r_{i} = (1 - b_{i})\mathbb{I}_{\left\{b_{i} > b_{-i}\right\}} +  y(1- b_{i})\mathbb{I}_{\left\{b_{i} = b_{-i}\right\}}$, where $y \sim$Ber$(0.5)$, then
+$$
+\begin{align*}
+	Q_{i} (b_{i}) &= \mathbb{E}\left[r_{i}| b_{i}, b_{-i}\right] + \gamma \mathbb{E}\left[\max_{b^{\prime}} Q_{i} (b^{\prime})\right]\\
+	&= \mathbb{E}\left[(1 - b_{i})\mathbb{I}_{\left\{b_{i} > b_{-i}\right\}} +  y(1- b_{i})\mathbb{I}_{\left\{b_{i} = b_{-i}\right\}} | b^{i}, b^{-i}\right] + \gamma \mathbb{E}\left[\max_{b^{\prime}} Q (b^{\prime})\right]\\
+	&= (1 - b_{i})\mathbb{I}_{\left\{b_{i} > b_{-i}\right\}} + (1- b_{i})\mathbb{I}_{\left\{b_{i} = b_{-i}\right\}}\mathbb{E}\left[ y \right] + \gamma \mathbb{E}\left[\max_{b^{\prime}} Q (b^{\prime})\right]\\
+	&= (1 - b_{i})\left[\mathbb{I}_{\left\{b_{i} > b_{-i}\right\}} + \frac{\mathbb{I}_{\left\{b_{i} = b_{-i}\right\}}}{2}\right] + \gamma \mathbb{E}\left[\max_{b^{\prime}} Q (b^{\prime})\right]\\
+\end{align*}
+$$
+
+But I woundn't go too far to state that the problem is a bandit one, I still think it is a RL problem. But unlike the authors, I think that the problem is mis-specified. First, the expectation in (1) is with respect to something. It could
 
 
